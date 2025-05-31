13b740db53da26bb7b8b6dbf5d9862d52032f98b
Anshul Pal <theanshul07@gmail.com>
2025-06-01 01:15:09 +0530

    Initial commit for Apna_bookStore project

---
+++ routes/bookRoutes.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+const Book = require('../models/bookSchema.js');
+
+// GET /books
+router.get('/', async (req, res) => {
+  try {
+    const books = await Book.find();
+    res.json(books);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// POST /books
+router.post('/', async (req, res) => {
+  try {
+    const newBook = new Book(req.body);
+    const savedBook = await newBook.save();
+    res.status(201).json(savedBook);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+// PUT /books/:id
+router.put('/:id', async (req, res) => {
+  try {
+    const updatedBook = await Book.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(updatedBook);
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+});
+
+// DELETE /books/:id
+router.delete('/:id', async (req, res) => {
+  try {
+    await Book.findByIdAndDelete(req.params.id);
+    res.json({ message: 'Book deleted' });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+module.exports = router;
